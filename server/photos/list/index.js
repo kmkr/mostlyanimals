5eb53d03e6d9be8e654aeebd1c36ec5b1160a11a
Kris-Mikael Krister <kmk@otovo.com>
2026-09-12 16:37:58 +0200

    Remove pointless file

--- server/photos/list/index.js
+++ server/photos/list/index.js
@@ -1,6 +1,5 @@
 const photos = require("../../../content.json");
-const photoDataFormatter = require("../photo-data-formatter");
 
 module.exports = () => {
-  return Promise.resolve(photoDataFormatter.dbToClient(photos));
+  return Promise.resolve(photos);
 };
