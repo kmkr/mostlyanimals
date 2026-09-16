2d7fdf77b25c0713e5ab67507d41a9e966d8f69b
Kris-Mikael Krister <kmk@otovo.com>
2026-09-16 14:11:25 +0200

    Remove most favicons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
-import Head from "next/head";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 import "../src/css/app.css";
 import "../src/polyfills";
@@ -13,30 +13,13 @@ function MyApp({ Component, pageProps }: AppProps) {
           name="google-site-verification"
           content="GV2uFZR4en6znRvzODbQEjF4vuUK9J2kAmGw7BwAbnE"
         />
-        <link
-          rel="icon"
-          type="image/png"
-          href="/images/favicon-100.png"
-          sizes="100x100"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/images/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          href="/images/favicon-192.png"
-          sizes="192x192"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          href="/images/favicon-512.png"
-          sizes="512x512"
-        />
         <link rel="icon" href="/images/favicon.ico" sizes="32x32" />
+        <link
+          rel="icon"
+          href="/images/logo_favicon.svg"
+          sizes="any"
+          type="image/svg+xml"
+        />
       </Head>
       <Component {...pageProps} />
       <script
